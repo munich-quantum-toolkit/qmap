064eef4a23744bf8ce501aa33542069cfca4401d
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-11-28 12:39:05 +0100

    🐛 Fix compilation bug on old mac OS

--- src/na/zoned/code_generator/CodeGenerator.cpp
+++ src/na/zoned/code_generator/CodeGenerator.cpp
@@ -241,8 +241,8 @@ auto CodeGenerator::RearrangementGenerator::addSourceMove(
     if (location != newLocation) {
       atomsToOffset.emplace_back(&atoms[qubit].get());
       offsetTargetLocations.emplace_back(
-          static_cast<double>(newLocation.first),
-          static_cast<double>(newLocation.second));
+          Location{.x = static_cast<double>(newLocation.first),
+                   .y = static_cast<double>(newLocation.second)});
       location = newLocation;
     }
   }
