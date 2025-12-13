e10eb24954e3a9e115ca5ad37e9b5066a0dbb322
Yannick Stade <100073938+ystade@users.noreply.github.com>
2025-12-13 11:34:33 +0100

    Revert "🎨 pre-commit fixes"
    This reverts commit aea1fe0d2d0aa9e0c4b35aa36fc808f8b211e091.

--- python/mqt/qmap/_compat/typing.py
+++ python/mqt/qmap/_compat/typing.py
@@ -8,7 +8,16 @@
 
 from __future__ import annotations
 
-from typing import Self
+import sys
+from typing import TYPE_CHECKING
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+elif TYPE_CHECKING:
+    from typing_extensions import Self
+else:
+    Self = object
+
 
 __all__ = ["Self"]
 
