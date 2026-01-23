76622e203bb24f79d8039840d85d1bc6d5a3c789
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-01-23 20:19:51 +0100

    Remove old __init__.py

--- python/mqt/qmap/na/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
-# Copyright (c) 2025 - 2026 Munich Quantum Software Company GmbH
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
-
-"""The MQT QMAP Neutral Atom Package."""
