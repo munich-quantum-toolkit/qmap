5c16615ba7fe4e06a811f8d6a1e2a6570a8ae2a8
Yannick Stade <100073938+ystade@users.noreply.github.com>
2026-04-03 11:21:49 +0200

    ✏️ Fix typo

--- include/na/zoned/decomposer/DecomposerBase.hpp
+++ include/na/zoned/decomposer/DecomposerBase.hpp
@@ -31,7 +31,7 @@ public:
    * meant to be first decomposed into the native gate set.
    * @param twoQubitGateLayers are the layers of two-qubit gates that the
    * decomposer may change.
-   * @return the a new pair of single-qubit and two-qubit gate layers
+   * @return a new pair of single-qubit and two-qubit gate layers
    */
   [[nodiscard]] virtual auto
   decompose(const std::vector<SingleQubitGateRefLayer>& singleQubitGateLayers,
